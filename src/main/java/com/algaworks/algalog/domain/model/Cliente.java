4e9a96723864d7f042fe6d35e261a36ea4764b16
Raul Souto <raul.soutos96@gmail.com>
2022-11-07 16:51:59 -0300

    configuração flyway + migrations + mysql e desenvolvimento de CRUD com HTTP

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,23 +1,33 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //utilizar forma nativa do BD - Auto Incrementar
 	private Long id;
 	private String nome;
 	private String email;
+	
+	@Column(name="fone")
 	private String telefone;
 	
-	public Cliente(Long id, String nome, String email, String telefone) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.telefone = telefone;
-	}
+	
+	
+
 	
 }
