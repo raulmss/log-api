cf3eb8c793fbfb093039fa6de67e2dcdc3fe435a
Raul Souto <raul.soutos96@gmail.com>
2022-11-10 00:28:32 -0300

    Criação de Service para segregação de regras de negócio

---
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -0,0 +1,37 @@
+package com.algaworks.algalog.domain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CatalogoClienteService {
+
+	private ClienteRepository clienteRepository;
+	
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+				.stream()
+				.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
+							//^Se o cliente encontrado no banco
+																		//^For diferente do cliente que foi passado como parâmetro
+										//Então email está em uso, portanto, operação não pode acontecer
+		if(emailEmUso) {
+			throw new NegocioException("Já existe um cliente cadastrado com este email.");
+		}
+		
+		return clienteRepository.save(cliente);
+	}
+	
+	@Transactional
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+}
